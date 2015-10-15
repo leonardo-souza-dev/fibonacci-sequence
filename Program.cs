d631f3170120bcf85b4448456faea5cc5684f279
unknown <leonardotreze@gmail.com>
2015-10-15 15:07:18 -0300

    app commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BigInteger> myList = new List<BigInteger>();
+
+            BigInteger j = 0;
+            myList.Add(j);
+
+            BigInteger temp = 0;
+            int counter = 0;
+
+            for (BigInteger i = 1; counter < 100; i = j + temp)
+            {
+                temp = j;
+                myList.Add(i);
+                j = i;
+                counter++;
+            }
+
+            Console.WriteLine(string.Join(", ", myList));
+            Console.ReadKey();
+        }
+    }
+}
